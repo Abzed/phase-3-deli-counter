a6fd2572168b49628a27207adfea161d0831f2ed
Abzed <ibraabzed@gmail.com>
2022-08-06 15:11:48 +0300

    commit tree fb32348008fa1dcc19f89189a350e7e844bef176

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+require "pry"
+
+katz_deli = []
+nums = [1,2,3]
+num = 0
+
+def line(katz_deli)
+    if katz_deli.size() == 0
+        puts "The line is currently empty."
+    else
+        # data = katz_deli.map{ | i | }
+        # puts "The line is currently: #{print('\n'.join(str(i) + ' ' + j for i, j in enumerate(data, 1)))}"
+        puts "The line is currently: #{katz_deli.map{ | i | i}.join(" ")}"
+    end
+end
+
+binding.pry
+
+
+
+
+
+
